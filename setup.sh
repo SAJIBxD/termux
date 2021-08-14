4103e4bd63b5263c0e7b976c72d7032badf692a1
Hossain Rizbi <88776229+HossainRizbi@users.noreply.github.com>
2021-08-15 04:50:32 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,25 @@
-pkg update && pkg upgrade
+#!/bin/bash
+
+clear 
+
+#this script will configure and install startup packages
+
+echo -e """
+\e[1;33mTermux Setup\e[1m 
+"""
+
+cd $HOME
+
+echo -e '\e[1;36m installing basic apps...\e[1m'
+
+pkg update && pkg upgrade 
+
+pkg install -y git
+
+pkg install -y neovim termux-api
+
+#setting desktop 
+cp -rf $HOME/termux-desktop-lxqt/.config $HOME
+
+termux-setup-storage
+ln -s $HOME/storage/music $HOME/Music
