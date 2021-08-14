c7981f4c5598a5c8e5b76b2656f069a8825e5015
Hossain Rizbi <88776229+HossainRizbi@users.noreply.github.com>
2021-08-15 05:23:51 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,9 +14,22 @@ echo -e '\e[1;36m installing basic apps...\e[1m'
 
 pkg update && pkg upgrade 
 
-pkg install -y git
+#zsh install & configuration
+pkg install zsh 
+chsh -s zsh
 
-pkg install -y htop neofetch fzf neovim termux-api
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+pkg install -y git htop neofetch fzf neovim termux-api
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Download zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+#Download zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
 
 #setting desktop 
 cp -rf $HOME/termux/.config $HOME
