99c3bb4d5e7596f5d9f79b1838471db498969d3d
Hossain Rizbi <88776229+HossainRizbi@users.noreply.github.com>
2021-11-01 13:50:48 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,7 +25,8 @@ setup () {
   pkg update && pkg upgrade 
   pkg install -y git htop neofetch fzf neovim termux-api
   termux-setup-storage
-}
+  
+}echo "${BLUE}Stroage Permission Granted${RESET}"
 
 install_mpd () {
   pkg i mpd ncmpcpp
@@ -33,13 +34,13 @@ install_mpd () {
 
 install_zsh_and_ohmyzsh () {
   pkg install zsh
-  
+  echo "${BLUE} Zsh Installed ${RESET}"
   # Change default shell to ZSH
   chsh -s zsh
-  
+  echo "${BLUE}Cloning Oh My Zsh...${RESET}"
   # Install oh-my-zsh
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-  
+  echo "${BLUE}Cloning Zsh Plugins...${RESET}"
   # Install zsh-autosuggestions plugin
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
@@ -51,8 +52,11 @@ install_zsh_and_ohmyzsh () {
 }
 
 install_other_dependencies () {
+  echo "${BLUE}Installing Python... ${RESET}"
   pkg install python
+  echo "${BLUE}Updating Pip...${RESET}"
   python -m pip install --upgrade pip
+  echo "${BLUE}Installing YouTube Downloader...${RESET}"
   pip install youtube-dl
 }
 
@@ -60,8 +64,9 @@ copy_files_and_create_dirs () {
   #mpd setup
   mkdir -p /data/data/com.termux/files/usr/var/run/
   mkdir -p ~/.config/mpd/playlists
-  
+  echo "${GREEN}MPD Setup completed${RESET}"
   cp -rf $SCRIPT_DIR/.config $HOME
+  echo "${GREEN}Config Restored${RESET}"
   cp -rf $SCRIPT_DIR/bin $HOME
 }
 setup_color
