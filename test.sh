886c779b83d7f7dd32db9c7f1c82413c092863c5
Hossain Rizbi <88776229+HossainRizbi@users.noreply.github.com>
2022-01-11 23:31:49 +0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 ### FUNCTIONS ###
-
 install_zsh() {
 	if ! [ -x "$(command -v zsh)" ]; then
 		echo -e "\\e[32m[ zsh ]\\e[m not found, installing"
@@ -17,21 +16,20 @@ install_zsh() {
 		fi
 	fi
 	
-	if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] && [ ! -d "${ZSH:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting" ]; then
+	if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ] && [ ! -d "$ZSH/plugins/zsh-syntax-highlighting" ]; then
 		if ask "\\e[32m[ oh-my-zsh ]\\e[m enable syntax highlighting?" Y; then
 			echo -e "\\e[32m[ oh-my-zsh ]\\e[m downloading plugin"
 			git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting --quiet >/dev/null
 		fi
-	else
+	#else
 		#sed -i '4s/.*/plugins=(git)/' "$HOME/.zshrc"
 	fi
 
-	if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] && [ ! -d "${ZSH:-~/.oh-my-zsh}/plugins/zsh-autosuggestions" ]; then
+	if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] && [ ! -d "$ZSH/plugins/zsh-autosuggestions" ]; then
 		if ask "\\e[32m[ oh-my-zsh ]\\e[m enable autosuggestions?" Y; then
 			echo -e "\\e[32m[ oh-my-zsh ]\\e[m downloading plugin"
 			git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions --quiet >/dev/null
-
-			#sed -i '4s/git/git zsh-autosuggestions/' "$HOME/.zshrc"
+#sed -i '4s/git/git zsh-autosuggestions/' "$HOME/.zshrc"
 		fi
 	fi
 
