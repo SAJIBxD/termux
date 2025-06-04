2ed9a7d49d3d0a67b1066e4a3cc1ef51b13f3db2
Hossain Rizbi <88776229+SAJIBxD@users.noreply.github.com>
2025-06-04 22:41:31 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,26 +2,38 @@
 
 pkg upgrade
 # Required packages
-pkg install -y git python fzf zsh exa rsync termux-api termux-tools ncurses-utils openssl-tool
+pkg install -y git python fzf zsh rsync termux-api termux-tools ncurses-utils openssl-tool
 # Optinal packages
-pkg install -y mpd mpc ncmpcpp cava vim ranger
+pkg install -y mpd mpc ncmpcpp cava vim ranger starship file yt-dlp eza
 
 # Storare permission
 termux-setup-storage
 
 # Pip packages
-pip install -U yt-dlp
+# pip install -U yt-dlp
 
 # Git repos
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Oh-My-Zsh install
+ZDOTDIR=$HOME/.config/zsh \
+ZSH=$HOME/.config/zsh/oh-my-zsh \
+RUNZSH=no \
+CHSH=no \
+KEEP_ZSHRC=yes \
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# ZSH plugins
+ZSH_CUSTOM=$HOME/.config/zsh/oh-my-zsh/custom
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Change default shell to ZSH
 chsh -s zsh
 
 # Dotfiles
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/HossainRizbi/termux.git tmpdotfiles
-rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
-rm -r tmpdotfiles
+# Copy all files except the .git folder
+cp -a tmpdotfiles/. "$HOME/"
+mv .zshrc $HOME/.config/zsh/
+
+# Cleanup
+rm -rf tmpdotfiles
